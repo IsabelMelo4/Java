75b05b0455e6d51703a722139553b5160e73d6dd
IsabelMelo4 <isabelmelo630@gmail.com>
2025-09-23 11:22:28 -0300

    Exercicio de arrays e arrays list

--- Arrays/src/ManipulandoArrays.java
+++ Arrays/src/ManipulandoArrays.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ManipulandoArrays {
+import java.util.ArrayList;
+
+public class ManipulandoArrays {
+
+    // criando um array
+
+    float[] array = new float[6]; // quando queremos definir um array sem elementos
+    int[] array2 = {1,2,3,4,5}; // quando definimos os valores do array
+
+    // um array por si só não pode ser alterado nem adicionar mais elementos nele
+    //para isso usamos o array list
+
+    public static void main(String[] args) {
+
+        ArrayList<Float> array3 = new ArrayList<Float>();
+        array3.add(3.5f);
+        array3.add(5.85f);
+        array3.add(9.85f); //adiciona elemento
+        System.out.println(array3);
+        System.out.println(array3.indexOf(5.85f)); // mostra o endereço do elemento
+        System.out.println(array3.get(2)); //mostra o elemento no indice indicado
+        array3.remove(5.85f); // remove o elemento
+
+        System.out.println(array3);
+
+    }
+
 }
--- Exercicios/src/ExercicioEstudanteeNota/Estudante.java
+++ Exercicios/src/ExercicioEstudanteeNota/Estudante.java
@@ -1,2 +1,26 @@
-package ExercicioEstudanteeNota;public class Estudante {
-}
+package ExercicioEstudanteeNota;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Estudante {
+        String nome;
+        long matricula;
+
+        float[] notas = new float[4];
+
+        //metodos
+
+        void inserirNotas() {
+
+            for(int i = 0; i < notas.length; i++) {
+
+                System.out.println("digite sua nota");
+                Scanner lerNota = new Scanner(System.in);
+                notas[i]= lerNota.nextFloat();
+
+            }
+
+                }
+        }
+
+
