29951e997c3fb272c564e15ca8b79f2b9830b313
PLM4 <pedrolucasmelo051@gmail.com>
2025-09-30 10:51:39 -0300

    Metodos getters, setters e construtor

---
+++ MetodosEspeciais/src/MetodosgGetters/Caneta.java
@@ -0,0 +1,63 @@
+package MetodosgGetters;
+
+public class Caneta {
+
+    public String marca;
+    private float ponta;
+
+    private String cor;
+
+    private boolean tampada;
+
+
+
+    // aqui definimos os atributos, onde eles podem ser publicos
+    // privados ou protegidos e nem todos podem acessar
+
+
+    // metodo construtor
+
+    public Caneta(String m, String c, float p){
+        this.marca = m;
+        this.cor = c;
+        this.ponta = p;
+        this.tampar();
+
+    }
+
+
+    public String getMarca(){
+        return this.marca;
+    }
+    public void setMarca(String m){
+        this.marca = m;
+    }
+
+
+    public float getPonta() {
+        return this.ponta;
+    }
+
+    public void setPonta(float p){
+        this.ponta = p;
+    }
+
+    public void tampar(){
+        this.tampada = true;
+    }
+
+
+    public void status(){
+        System.out.println("sobre a caneta");
+        System.out.println("Modelo"+this.marca);
+        System.out.println("Ponta" + this.ponta);
+        System.out.println("cor" + this.cor);
+        System.out.println("tampada" + this.tampada);
+
+
+
+    }
+
+
+}
+
---
+++ MetodosEspeciais/src/MetodosgGetters/Main.java
@@ -0,0 +1,16 @@
+package MetodosgGetters;
+
+public class Main {
+    public static void main(String[] args) {System.out.println();
+
+        Caneta c1 = new Caneta("bic", "azul", 5.6f);
+        c1.status();
+
+      /*  c1.setMarca("bic");
+        c1.status();
+
+        System.out.println("tenho uma ponta" + c1.getPonta());
+        System.out.println("com o modelo" + c1.getMarca());*/
+
+    }
+}
